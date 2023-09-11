042d5ade699630ce34804729e939653ae26b22ac
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-09-11 23:37:28 +0300

    Fixing deployment issues

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <LangProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<LandingPage/>} />
             <Route path="/contact" element={<Contact/>} />
